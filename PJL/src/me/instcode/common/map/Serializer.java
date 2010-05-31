0923f7bed88707f74b7f53da0e9086e45cbec93f
instcode <instcode@b27b4286-dfbc-11de-8dcd-9b777bee35ae>
2010-05-31 12:40:31 +0000

    Add a simple example how to use this Serializer class

--- PJL/src/me/instcode/common/map/Serializer.java
+++ PJL/src/me/instcode/common/map/Serializer.java
@@ -4,20 +4,50 @@ import java.nio.ByteBuffer;
 import java.util.Map.Entry;
 
 /**
- * To prevent buffer underflow, make sure you have the correct record
- * length. Another way is to manually update the buffer position as
- * following:
+ * Read/Write object from/to the given buffer.
+ * 
+ * Examples:
+ * <pre>
+ * class TripSerializer implements Serializer&lt;Long, Trip&gt; {
+ * 	&#064;Override
+ * 	public Entry&lt;Long, Trip&gt; readObject(ByteBuffer buffer) {
+ * 		Trip trip = new Trip();
+ * 		Long key = Long.valueOf(buffer.getLong());
+ * 		trip.count = buffer.getLong();
+ * 		trip.sumFare = buffer.getLong();
+ * 		trip.sumTime = buffer.getLong();
+ * 		return new AbstractMap.SimpleEntry&lt;Long, Trip&gt;(key, trip);
+ * 	}
+ * 
+ * 	&#064;Override
+ * 	public void writeObject(Entry&lt;Long, Trip&gt; value, ByteBuffer buffer) {
+ * 		Trip trip = value.getValue();
+ * 		buffer.putLong(value.getKey().longValue());
+ * 		buffer.putLong(trip.count);
+ * 		buffer.putLong(trip.sumFare);
+ * 		buffer.putLong(trip.sumTime);
+ * 	}
+ * 
+ * 	&#064;Override
+ * 	public int getRecordLength() {
+ * 		return 32;
+ * 	}
+ * }
+ * </pre>
+ *  * To prevent buffer underflow, make sure you have a correct record length.
+ * Another way is to manually update the buffer position as following:
  * 
  * <pre>
  * int pos = buffer.position();
  * // ... your serialization code goes here...
  * buffer.position(pos + getRecordSize());
- * </pre>
- * 
+ * </pre>  
  * @author instcode
  * 
- * @param <K> Key
- * @param <V> Value
+ * @param <K>
+ *            Key
+ * @param <V>
+ *            Value
  */
 public interface Serializer<K, V> {
 
